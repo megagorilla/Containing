4e2a25b06b856dc57a73efb711e27664ede75ceb
megagorilla <sanderenbart@gmail.com>
2014-11-13 21:03:25 +0100

    right, water werkt nu

--- ContainingDisplay/src/Controller/DisplayController.java
+++ ContainingDisplay/src/Controller/DisplayController.java
@@ -20,6 +20,7 @@ import com.jme3.util.SkyFactory;
 public class DisplayController extends SimpleApplication {
 Train train1;
 TexturedMaterial trainMat;
+DirectionalLight sun;
 
     public static void main(String[] args) {
         DisplayController app = new DisplayController();
@@ -37,11 +38,13 @@ TexturedMaterial trainMat;
         train1 = new Train(new Vector3f(10, 10, 10),"Treintjeeee" , assetManager, trainMat);
         rootNode.attachChild(train1);
             /** A white, directional light source */ 
-        DirectionalLight sun = new DirectionalLight();
+        sun = new DirectionalLight();
         sun.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)).normalizeLocal());
         sun.setColor(ColorRGBA.White.clone().multLocal(2));
         rootNode.addLight(sun);
+        
         SeaNode sea = new SeaNode(this);
+        sea.setLocalTranslation(0, -100, 0);
         rootNode.attachChild(sea);
     }
 
@@ -50,6 +53,12 @@ TexturedMaterial trainMat;
         train1.rotate(0f, 0.003f, 0f);
     }
 
+    public DirectionalLight getSun() {
+        return sun;
+    }
+
+    
+    
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
--- ContainingDisplay/src/Scenery/SeaNode.java
+++ ContainingDisplay/src/Scenery/SeaNode.java
@@ -6,6 +6,7 @@ package Scenery;
 
 import Controller.DisplayController;
 import com.jme3.asset.AssetManager;
+import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
 import com.jme3.math.Plane;
 import com.jme3.math.Quaternion;
@@ -15,6 +16,7 @@ import com.jme3.renderer.ViewPort;
 import com.jme3.renderer.queue.RenderQueue.ShadowMode;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Quad;
 import com.jme3.water.SimpleWaterProcessor;
 
@@ -25,31 +27,20 @@ import com.jme3.water.SimpleWaterProcessor;
 public class SeaNode extends Node{
 
     public SeaNode(DisplayController main) {
-        // we create a water processor
+        //create processor
         SimpleWaterProcessor waterProcessor = new SimpleWaterProcessor(main.getAssetManager());
-        waterProcessor.setReflectionScene(this);
- 
-        // we set the water plane
-        Vector3f waterLocation=new Vector3f(0,-6,0);
-        waterProcessor.setPlane(new Plane(Vector3f.UNIT_Y, waterLocation.dot(Vector3f.UNIT_Y)));
+        waterProcessor.setReflectionScene(main.getRootNode());
         main.getViewPort().addProcessor(waterProcessor);
- 
-        // we set wave properties
-        waterProcessor.setWaterDepth(40);         // transparency of water
-        waterProcessor.setDistortionScale(0.05f); // strength of waves
-        waterProcessor.setWaveSpeed(0.05f);       // speed of waves
- 
-        // we define the wave size by setting the size of the texture coordinates
-        Quad quad = new Quad(400,400);
-        quad.scaleTextureCoordinates(new Vector2f(6f,6f));
- 
-        // we create the water geometry from the quad
-        Geometry water=new Geometry("water", quad);
-        water.setLocalRotation(new Quaternion().fromAngleAxis(-FastMath.HALF_PI, Vector3f.UNIT_X));
-        water.setLocalTranslation(-200, -6, 250);
-        water.setShadowMode(ShadowMode.CastAndReceive);
-        water.setMaterial(waterProcessor.getMaterial());
-        attachChild(water);
+
+        waterProcessor.setLightPosition(main.getSun().getDirection());
+
+        //create water quad
+        Spatial waterPlane=(Spatial)  main.getAssetManager().loadModel("Models/WaterTest/WaterTest.mesh.xml");
+        waterPlane.setMaterial(waterProcessor.getMaterial());
+        waterPlane.setLocalScale(2000);
+        waterPlane.setLocalTranslation(-5, 0, 5);
+
+        attachChild(waterPlane);
     }
     
 }
