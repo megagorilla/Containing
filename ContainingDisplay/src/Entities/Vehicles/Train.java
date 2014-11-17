970cf23aeca07f79bfb8d6beab5d82747ee58a9a
YannickStrobl <yannick-strobl@live.nl>
2014-11-17 14:43:36 +0100

    Trein klaar

--- ContainingDisplay/src/Entities/Vehicles/Train.java
+++ ContainingDisplay/src/Entities/Vehicles/Train.java
@@ -4,19 +4,73 @@
  */
 package Entities.Vehicles;
 
+import Controller.DisplayController;
+import static Controller.DisplayController.Quality.HIGH;
+import static Controller.DisplayController.Quality.LOW;
+import static Controller.DisplayController.Quality.MEDIUM;
 import Materials.TexturedMaterial;
 import com.jme3.asset.AssetManager;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
 
 /**
  *
  * @author Sander
  */
-public class Train extends Vehicle{
-
-    public Train(Vector3f location, String name, AssetManager assetManager, TexturedMaterial material) {
-        super(location, name,ColorRGBA.Yellow, new Vector3f(10, 10, 10), assetManager, material);
+public class Train extends Vehicle
+{
+    Node trainNode;
+    Node wagonNode;
+    
+    public Train(DisplayController.Quality quality, DisplayController main) 
+    {
+        super();
+        String qualityPath = "";
+        switch (quality)
+        {
+            case HIGH:
+                qualityPath = "Models/high/train/train.j3o";
+                break;
+            case MEDIUM:
+                qualityPath = "Models/medium/train/train.j3o";
+                break;
+            case LOW:
+                qualityPath = "Models/low/train/train.j3o";
+                break;
+        }
+        trainNode = (Node)main.getAssetManager().loadModel(qualityPath);
+        attachChild(trainNode);
+        main.getRootNode().attachChild(this);
+        
+        //trainNode.setLocalTranslation(0, 0, 0);
+        Wagon(quality, main);
+    }
+    
+    public void Wagon(DisplayController.Quality quality, DisplayController main)
+    {
+        for(int i = 0; i < 5; i++)
+        {
+        String qualityPath = "";
+        switch (quality)
+        {
+            case HIGH:
+                qualityPath = "Models/high/train/wagon.j3o";
+                break;
+            case MEDIUM:
+                qualityPath = "Models/medium/train/wagon.j3o";
+                break;
+            case LOW:
+                qualityPath = "Models/low/train/wagon.j3o";
+                break;
+        }
+        wagonNode = (Node)main.getAssetManager().loadModel(qualityPath);
+        attachChild(wagonNode);
+        
+        
+        wagonNode.setLocalTranslation(0, 0, -13.2f - 18.4f*i);
+        }
+        main.getRootNode().attachChild(this);
     }
     
 }
