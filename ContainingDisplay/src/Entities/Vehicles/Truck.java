ad800ea51eef98e43966dfe6651e37a4f4aa5772
YannickStrobl <yannick-strobl@live.nl>
2014-11-17 14:12:47 +0100

    Truck klaar

--- ContainingDisplay/src/Entities/Vehicles/Truck.java
+++ ContainingDisplay/src/Entities/Vehicles/Truck.java
@@ -4,17 +4,49 @@
  */
 package Entities.Vehicles;
 
+import Controller.DisplayController;
 import com.jme3.asset.AssetManager;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
 
 /**
  *
  * @author Sander
  */
-public class Truck extends Vehicle{
-
+public class Truck extends Vehicle
+{
+    Node truckNode;
     
+    public Truck(DisplayController.Quality quality, DisplayController main)
+    {
+        super();
+        String qualityPath = "Models/high/truck.j3o";
+        switch (quality)
+        {
+            case HIGH:
+                qualityPath = "Models/high/truck.j3o";
+                break;
+            case MEDIUM:
+                qualityPath = "Models/medium/truck.j3o";
+                break;
+            case LOW:
+                qualityPath = "Models/low/truck.j3o";
+                break;
+        }
+        truckNode = (Node)main.getAssetManager().loadModel(qualityPath);
+        attachChild(truckNode);
+        main.getRootNode().attachChild(this);
+    }
     
+    public void moveTruck()
+    {
+        truckNode.setLocalTranslation(1, 0, 0);
+    }
+    
+    public Vector3f getTruckPosition()
+    {
+        return this.getLocalTranslation();
+    }    
     
 }
