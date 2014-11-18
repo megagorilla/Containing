6827361e89a274b040057989a79821b2c32982e0
megagorilla <sanderenbart@gmail.com>
2014-11-18 10:09:12 +0100

    train en truckcrane zitten er ook in

--- ContainingDisplay/src/Entities/Cranes/TrainCrane.java
+++ ContainingDisplay/src/Entities/Cranes/TrainCrane.java
@@ -4,10 +4,62 @@
  */
 package Entities.Cranes;
 
+import Controller.DisplayController;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import sun.awt.DisplayChangedListener;
+
 /**
  *
  * @author Sander
  */
 public class TrainCrane extends Crane{
+
+    public TrainCrane(DisplayController main) {
+        super();
+        try{
+            Node craneBase = (Node)main.getAssetManager().loadModel("Models/high/crane/traincrane/crane.j3o");
+            attachChild(craneBase);
+        }catch(Exception e){
+            Geometry craneBaseGeom = (Geometry)main.getAssetManager().loadModel("Models/high/crane/traincrane/crane.j3o");
+            attachChild(craneBaseGeom);
+        }
+        
+        try{
+            Node grabbingGear = (Node)main.getAssetManager().loadModel("Models/high/crane/traincrane/grabbingGear.j3o");
+            grabber.attachChild(grabbingGear);
+        }catch(Exception e){
+            Geometry grabbingGearGeom = (Geometry)main.getAssetManager().loadModel("Models/high/crane/traincrane/grabbingGear.j3o");
+            grabber.attachChild(grabbingGearGeom);
+        }
+        
+        try{
+            Node grabbingGearHolder = (Node)main.getAssetManager().loadModel("Models/high/crane/traincrane/grabbingGearHolder.j3o");
+            grabber.attachChild(grabbingGearHolder);
+        }catch(Exception e){
+            Geometry grabbingGearHolderGeom = (Geometry)main.getAssetManager().loadModel("Models/high/crane/traincrane/grabbingGearHolder.j3o");
+            grabber.attachChild(grabbingGearHolderGeom);
+        }
+        
+        try{
+            Node hookLeft = (Node)main.getAssetManager().loadModel("Models/high/crane/traincrane/hookLeft.j3o");
+            grabber.attachChild(hookLeft);
+        }catch(Exception e){
+            Geometry hookLeftGeom = (Geometry)main.getAssetManager().loadModel("Models/high/crane/traincrane/hookLeft.j3o");
+            grabber.attachChild(hookLeftGeom);
+        }
+        
+        try{
+            Node hookRight = (Node)main.getAssetManager().loadModel("Models/high/crane/traincrane/hookRight.j3o");
+            grabber.attachChild(hookRight);
+        }catch(Exception e){
+            Geometry hookRightGeom = (Geometry)main.getAssetManager().loadModel("Models/high/crane/traincrane/hookRight.j3o");
+            grabber.attachChild(hookRightGeom);
+        }
+        
+        attachChild(grabber);
+        main.getRootNode().attachChild(this);
+    }
+    
     
 }
--- ContainingDisplay/src/Entities/Cranes/TruckCrane.java
+++ ContainingDisplay/src/Entities/Cranes/TruckCrane.java
@@ -4,10 +4,51 @@
  */
 package Entities.Cranes;
 
+import Controller.DisplayController;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+
 /**
  *
  * @author Sander
  */
-public class TruckCrane extends Crane{
-    
+public class TruckCrane extends Crane {
+
+    public TruckCrane(DisplayController main) {
+        super();
+        try {
+            Node craneBase = (Node) main.getAssetManager().loadModel("Models/high/crane/truckcrane/crane.j3o");
+            attachChild(craneBase);
+        } catch (Exception e) {
+            Geometry craneBaseGeom = (Geometry) main.getAssetManager().loadModel("Models/high/crane/truckcrane/crane.j3o");
+            attachChild(craneBaseGeom);
+        }
+
+        try {
+            Node grabbingGear = (Node) main.getAssetManager().loadModel("Models/high/crane/truckcrane/grabbingGear.j3o");
+            grabber.attachChild(grabbingGear);
+        } catch (Exception e) {
+            Geometry grabbingGearGeom = (Geometry) main.getAssetManager().loadModel("Models/high/crane/truckcrane/grabbingGear.j3o");
+            grabber.attachChild(grabbingGearGeom);
+        }
+
+        try {
+            Node hookLeft = (Node) main.getAssetManager().loadModel("Models/high/crane/truckcrane/hookLeft.j3o");
+            grabber.attachChild(hookLeft);
+        } catch (Exception e) {
+            Geometry hookLeftGeom = (Geometry) main.getAssetManager().loadModel("Models/high/crane/truckcrane/hookLeft.j3o");
+            grabber.attachChild(hookLeftGeom);
+        }
+
+        try {
+            Node hookRight = (Node) main.getAssetManager().loadModel("Models/high/crane/truckcrane/hookRight.j3o");
+            grabber.attachChild(hookRight);
+        } catch (Exception e) {
+            Geometry hookRightGeom = (Geometry) main.getAssetManager().loadModel("Models/high/crane/truckcrane/hookRight.j3o");
+            grabber.attachChild(hookRightGeom);
+        }
+
+        attachChild(grabber);
+        main.getRootNode().attachChild(this);
+    }
 }
