ff270153d0219a159db079ff484a06eb29333d67
megagorilla <sanderenbart@gmail.com>
2014-11-25 10:07:21 +0100

    some new classes

---
+++ ContainingController/src/nhl/containing/server/entities/cranes/StorageCrane.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nhl.containing.server.entities.cranes;
+
+/**
+ *
+ * @author Sander
+ */
+public class StorageCrane {
+    
+}
