725ddb47b1e1f532aa72b4198307cfb15039a65b
megagorilla <sanderenbart@gmail.com>
2014-11-25 13:58:10 +0100

    solved some merging problems in ContainingClient not all platforms were created

--- ContainingDisplay/src/nhl/containing/client/ContainingClient.java
+++ ContainingDisplay/src/nhl/containing/client/ContainingClient.java
@@ -2,27 +2,23 @@ package nhl.containing.client;
 
 import nhl.containing.client.entities.Platform;
 import nhl.containing.client.entities.platforms.BinnenvaartschipPlatform;
-import nhl.containing.client.entities.platforms.TrainPlatform;
 import nhl.containing.client.entities.vehicles.AGV;
 import nhl.containing.client.network.ConnectionManager;
 import nhl.containing.client.scenery.SeaNode;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.asset.AssetManager;
-import com.jme3.cinematic.MotionPath;
 import com.jme3.cinematic.events.MotionEvent;
 import com.jme3.light.DirectionalLight;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.Quaternion;
+import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.scene.Node;
 import com.jme3.util.SkyFactory;
-import com.sun.org.apache.bcel.internal.generic.FASTORE;
 import java.util.ArrayList;
 import nhl.containing.client.ContainingClient.Quality;
-import nhl.containing.client.entities.Container;
 import nhl.containing.client.entities.cranes.StorageCrane;
 import nhl.containing.client.entities.cranes.TruckCrane;
 import nhl.containing.client.entities.platforms.OpslagPlatform;
@@ -36,77 +32,71 @@ import nhl.containing.client.entities.platforms.TruckPlatform;
  * @author Yannick
  */
 public class ContainingClient extends SimpleApplication {
-
+    
     private DirectionalLight sun;
-
+    
     public enum Quality {
-
+        
         LOW, MEDIUM, HIGH
     };
-	 private Quality quality = Quality.HIGH;
+    private Quality quality = Quality.HIGH;
     private static Node myRootNode;
     private static AssetManager myAssetManager;
     private static ViewPort myViewPort;
-	MotionEvent motionControl;
-	public static AGV agv;
-        ArrayList<StorageCrane> StorageCranes = new ArrayList<StorageCrane>();
-        ArrayList<Platform> Platforms = new ArrayList<Platform>();
-        
-
+    MotionEvent motionControl;
+    public static AGV agv;
+    ArrayList<StorageCrane> StorageCranes = new ArrayList<StorageCrane>();
+    ArrayList<TruckCrane> TruckCranes = new ArrayList<TruckCrane>();
+    ArrayList<Platform> Platforms = new ArrayList<Platform>();
     Node rails;
-
+    
     public static void main(String[] args) {
         ContainingClient app = new ContainingClient();
         app.start();
     }
-
+    
     @Override
     public void simpleInitApp() {
         myAssetManager = assetManager;
         myRootNode = rootNode;
         myViewPort = viewPort;
         rootNode.attachChild(SkyFactory.createSky(assetManager, "Textures/Sky/Bright/BrightSky.dds", false));
-        flyCam.setMoveSpeed(100);
+        flyCam.setMoveSpeed(200);
         cam.setFrustumFar(5000);
         cam.onFrameChange();
-		
-		agv = new AGV(Quality.HIGH);
-		
-		ConnectionManager.init("localhost", 3000);
+        
+        agv = new AGV(quality);
+        
+        ConnectionManager.init("localhost", 3000);
 
-        //Platform test = new BinnenvaartschipPlatform();
-		//Platform test1 = new TrainPlatform();
-		//test1.setLocalTranslation(100, 0, 0);
         sun = new DirectionalLight();
         sun.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)).normalizeLocal());
         sun.setColor(ColorRGBA.White);
         rootNode.addLight(sun);        
-		
+        
         SeaNode sea = new SeaNode();
-                
-        //this.getRootNode().attachChild(test);
-        //this.getRootNode().attachChild(sea);
-		//this.getRootNode().attachChild(sea);
-                Platforms.add(new TrainPlatform());                        
-                Platforms.add(new TruckPlatform());                
-		Platforms.add(new BinnenvaartschipPlatform());
-                
-                
-                for(int i = 0; i < 20; i++)
-                {
-                    TruckCranes.add(new TruckCrane());
-                    TruckCranes.get(i).setLocalTranslation(360, 0, -750 + 25*i);
-                    TruckCranes.get(i).rotate(0, FastMath.HALF_PI, 0);
-                }
-                
-                for(int i = 0; i < 39; i++)
-                {
-                    StorageCranes.add(new StorageCrane());
-                    StorageCranes.get(i).setLocalTranslation(0, 0, -760 + 40*i);
-                    StorageCranes.get(i).rotate(0, FastMath.HALF_PI, 0);
-                }
+        this.getRootNode().attachChild(sea);
+        
+        Platforms.add(new OpslagPlatform());
+        Platforms.add(new SeaShipPlatform());
+        Platforms.add(new TrainPlatform());        
+        Platforms.add(new TruckPlatform());        
+        Platforms.add(new BinnenvaartschipPlatform());
+        
+        
+        for (int i = 0; i < 20; i++) {
+            TruckCranes.add(new TruckCrane());
+            TruckCranes.get(i).setLocalTranslation(360, 0, -750 + 25 * i);
+            TruckCranes.get(i).rotate(0, FastMath.HALF_PI, 0);
+        }
+        
+        for (int i = 0; i < 39; i++) {
+            StorageCranes.add(new StorageCrane());
+            StorageCranes.get(i).setLocalTranslation(0, 0, -760 + 40 * i);
+            StorageCranes.get(i).rotate(0, FastMath.HALF_PI, 0);
+        }
     }
-
+    
     @Override
     public void simpleUpdate(float tpf) {
     }
@@ -140,11 +130,11 @@ public class ContainingClient extends SimpleApplication {
     public static ViewPort getMyViewPort() {
         return myViewPort;
     }
-
+    
     public DirectionalLight getSun() {
         return sun;
     }
-
+    
     @Override
     public void simpleRender(RenderManager rm) {
         // TODO: add render code
