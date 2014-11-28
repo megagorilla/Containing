038f6f8e6b94a4cdfb04b7c18ba74b0f8c221e89
megagorilla <sanderenbart@gmail.com>
2014-11-28 10:56:30 +0100

    low quality container enabled

--- ContainingDisplay/src/nhl/containing/client/entities/Container.java
+++ ContainingDisplay/src/nhl/containing/client/entities/Container.java
@@ -12,40 +12,29 @@ import com.jme3.renderer.queue.RenderQueue;
 import com.jme3.scene.Node;
 
 /**
- * 
+ *
  * @author Yannick
  */
-public class Container extends Node
-{
-	Node containerNode;
+public class Container extends Node {
 
-	public Container(ContainingClient.Quality qualtiy)
-	{
-		String modelPath = "Models/high/container/container.j3o";
-		switch (qualtiy)
-		{
-			case LOW:
-				modelPath = "Models/low/container/container.j3o";
-				break;
-			case MEDIUM:
-				modelPath = "Models/medium/container/container.j3o";
-				break;
-			case HIGH:
-				modelPath = "Models/high/container/container.j3o";
-				break;
-		}
-		try
-		{
-			containerNode = (Node) ContainingClient.getMyAssetManager().loadModel(modelPath);
-		}
-		catch (Exception e)
-		{
-			containerNode = (Node) ContainingClient.getMyAssetManager().loadModel("Models/medium/container/container.j3o");
-		}
+    Node containerNode = new Node();
 
-		containerNode.setMaterial(new PlainMaterial(ColorRGBA.randomColor()));
-		attachChild(containerNode);
-		ContainingClient.getMyRootNode().attachChild(this);
-	}
-
+    public Container(ContainingClient.Quality qualtiy) {
+        String modelPath = "Models/high/container/container.j3o";
+        switch (qualtiy) {
+            case LOW:
+                modelPath = "Models/low/container/container.j3o";
+                break;
+            case MEDIUM:
+                modelPath = "Models/medium/container/container.j3o";
+                break;
+            case HIGH:
+                modelPath = "Models/high/container/container.j3o";
+                break;
+        }
+        containerNode.attachChild(ContainingClient.getMyAssetManager().loadModel(modelPath));
+        containerNode.setMaterial(new PlainMaterial(ColorRGBA.randomColor()));
+        attachChild(containerNode);
+        ContainingClient.getMyRootNode().attachChild(this);
+    }
 }
