6fcb4cec0e6bee5348f3f4546e90afc8d9b8d6dd
megagorilla <sanderenbart@gmail.com>
2014-12-04 10:19:33 +0100

    zodat Arjen niet meer hoef te mierenneuken :kissing_heart:

--- ContainingDisplay/src/nhl/containing/client/entities/platforms/TruckPlatform.java
+++ ContainingDisplay/src/nhl/containing/client/entities/platforms/TruckPlatform.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package nhl.containing.client.entities.platforms;
 
 import com.jme3.material.Material;
--- ContainingDisplay/src/nhl/containing/client/materials/PlainMaterial.java
+++ ContainingDisplay/src/nhl/containing/client/materials/PlainMaterial.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package nhl.containing.client.materials;
 
 import nhl.containing.client.ContainingClient;
