87e57edaef50afaf4ef0140a4d06c794b57e790a
megagorilla <sanderenbart@gmail.com>
2014-12-09 13:03:57 +0100

    Loading of containers in containingserver (xml7) and placed in different lists. System.gc(); is forced garbage collection. the xml loader uses a lot of ram that otherwise would use 700MB ram. after forced GC there's only 60MB ram usage over

--- ContainingController/src/nhl/containing/server/ContainingServer.java
+++ ContainingController/src/nhl/containing/server/ContainingServer.java
@@ -11,12 +11,21 @@ import nhl.containing.server.util.ControlHandler;
 import com.jme3.app.SimpleApplication;
 import com.jme3.scene.Node;
 import com.jme3.system.JmeContext;
+import java.util.ArrayList;
+import nhl.containing.server.util.XMLFileReader;
+import nhl.containing.server.util.XMLFileReader.*;
 
 public class ContainingServer extends SimpleApplication
 {
 	float time = 0;
 	private boolean hasSent;
 	private static Node staticRootNode;
+        
+        ArrayList<Container> containers;
+        ArrayList<Container> seaShipContainers = new ArrayList<>();
+        ArrayList<Container> riverShipContainers = new ArrayList<>();
+        ArrayList<Container> trainContainers = new ArrayList<>();
+        ArrayList<Container> truckContainers = new ArrayList<>();
 
 	/**
 	 * Starts the app headless (no display)
@@ -43,7 +52,26 @@ public class ContainingServer extends SimpleApplication
 		AGVHandler.getInstance().init();
 		ConnectionManager.initialize(3000);
 		API.start(8080);
-		
+                
+                XMLFileReader instance = new XMLFileReader();
+		containers = instance.getContainers("../XMLFILES/xml7.xml");
+                for(Container c : containers){
+                    switch (c.getArrival().getType()){
+                        case BINNENSCHIP:
+                            riverShipContainers.add(c);
+                            break;
+                        case TREIN:
+                            trainContainers.add(c);
+                            break;
+                        case VRACHTAUTO:
+                            truckContainers.add(c);
+                            break;
+                        case ZEESCHIP:
+                            seaShipContainers.add(c);
+                            break;
+                    }
+                }
+                System.gc();
         System.out.println("1");
 	}
 
