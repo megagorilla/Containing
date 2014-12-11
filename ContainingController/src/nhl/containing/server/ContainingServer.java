dbc04b76d2405ec584512ddd92dc21925db2cdb8
megagorilla <sanderenbart@gmail.com>
2014-12-11 17:52:58 +0100

    oops. removed not needed code

--- ContainingController/src/nhl/containing/server/ContainingServer.java
+++ ContainingController/src/nhl/containing/server/ContainingServer.java
@@ -141,47 +141,6 @@ public class ContainingServer extends SimpleApplication
         //forced garbage collection (reduces RAM usage from 700MB  to 60 MB in case of xml7)
         System.gc();
     }
-
-//        /**
-//         * retrieves arraylist for the next seaship and removes the containers
-//         * from the list of all the seashipcontainers
-//         * @return An arraylist of containers that fit in 1 seaship
-//         */
-//        public ArrayList<Container> getNextSeaShip() {
-//        ArrayList<Container> containters = new ArrayList<>();
-//        containters.add(seaShipContainers.get(0));
-//        seaShipContainers.remove(0);
-//        boolean shipFull = false;
-//        while (!shipFull && seaShipContainers.size()>0) {
-//            if (seaShipContainers.get(0).getPositie().equals(new Vector3f(0.0f, 0.0f, 0.0f))) {
-//                shipFull = true;
-//            } else {
-//                containters.add(seaShipContainers.get(0));
-//                seaShipContainers.remove(0);
-//            }
-//        }
-//        return containters;
-//    }
-//        /**
-//         * retrieves arraylist for the next train and removes the containers
-//         * from the list of all the trainContainers
-//         * @return An arraylist of containers that fit in 1 train
-//         */
-//        public ArrayList<Container> getNextTrain() {
-//            ArrayList<Container> containters = new ArrayList<>();
-//            containters.add(trainContainers.get(0));
-//            trainContainers.remove(0);
-//            boolean trainFull = false;
-//            while (!trainFull && trainContainers.size()>0) {
-//                if (trainContainers.get(0).getPositie().equals(new Vector3f(0.0f, 0.0f, 0.0f))) {
-//                    trainFull = true;
-//                }else{
-//                    containters.add(trainContainers.get(0));
-//                    trainContainers.remove(0);
-//                }
-//            }
-//            return containters;
-//        }
         
 	/**
 	 * Updates the server every frame
