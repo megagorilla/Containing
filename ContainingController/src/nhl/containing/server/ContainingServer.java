5c0d5d598591c20f978d6adad93271be55af6c26
megagorilla <sanderenbart@gmail.com>
2014-12-15 13:05:34 +0100

    seaship and barge containers are put in stacks of 2d arrays of stacks of containers

--- ContainingController/src/nhl/containing/server/ContainingServer.java
+++ ContainingController/src/nhl/containing/server/ContainingServer.java
@@ -26,12 +26,12 @@ public class ContainingServer extends SimpleApplication
         
         private int currentDay = 1;
         private float dayCounter = 0;
-        private final float dayLength = 1f; //the time 1 gameday should be in seconds
+        private final float dayLength = 30f; //the time 1 gameday should be in seconds
         long startTime;
         
         ArrayList<Container> containers;
-        Stack<Stack<Container>> listofSeaShipContainers = new Stack<>();
-        Stack<Stack<Container>> listofriverShipContainers = new Stack<>();
+        Stack<Stack<Container>[][]> listofSeaShipContainers = new Stack<>();
+        Stack<Stack<Container>[][]> listofBargeConatiners = new Stack<>();
         Stack<Stack<Container>> listoftrainContainers = new Stack<>();
         Stack<Container> listoftruckContainers = new Stack<>();
 
@@ -51,6 +51,7 @@ public class ContainingServer extends SimpleApplication
 	@Override
 	public void simpleInitApp()
 	{
+            startTime = System.currentTimeMillis();
 		staticRootNode = this.getRootNode();
 		new RouteController();
 		new ControlHandler();
@@ -63,7 +64,7 @@ public class ContainingServer extends SimpleApplication
                 
             initContainers();
             System.out.println("1");
-            startTime = System.currentTimeMillis();
+            
 	}
         
         /**
@@ -91,24 +92,43 @@ public class ContainingServer extends SimpleApplication
                     break;
             }
         }
-        
+        float highestx = 0;
+        float highesty = 0;
+        float highestz = 0;
+        for(Container c :  riverShipContainers){
+            if(c.getPositie().x > highestx)
+                highestx = c.getPositie().x;
+            if(c.getPositie().y > highesty)
+                highesty = c.getPositie().y;
+            if(c.getPositie().z > highestz)
+                highestz = c.getPositie().z;
+        }
         boolean isFull;
         while(riverShipContainers.size()>0){
-            listofriverShipContainers.add(new Stack<>());
-            listofriverShipContainers.get(listofriverShipContainers.size()-1).push(riverShipContainers.get(0));
+            listofBargeConatiners.add((Stack<Container>[][]) new Stack[13][3]);
+           for(int i = 0; i<13;i++){
+               for(int j = 0; j< 3;j++){
+                   listofBargeConatiners.get(listofBargeConatiners.size()-1)[i][j] = new Stack<>();
+               }
+           }
+           int x = (int)riverShipContainers.get(0).getPositie().x;
+           int z = (int)riverShipContainers.get(0).getPositie().z;
+            listofBargeConatiners.get(listofBargeConatiners.size()-1)[x][z].push(riverShipContainers.get(0));
             riverShipContainers.remove(0);
             isFull = false;
             while(riverShipContainers.size()>0 && !isFull){
                 if(riverShipContainers.get(0).getPositie().equals(Vector3f.ZERO)){
                     isFull = true;
                 }else{
-                    listofriverShipContainers.get(listofriverShipContainers.size()-1).push(riverShipContainers.get(0));
+                    x = (int)riverShipContainers.get(0).getPositie().x;
+                    z = (int)riverShipContainers.get(0).getPositie().z;
+                    listofBargeConatiners.get(listofBargeConatiners.size()-1)[x][z].push(riverShipContainers.get(0));
                     riverShipContainers.remove(0);
                 }
             }
         }
-        Collections.sort(listofriverShipContainers, (a,b) -> ((a.get(0).getArrival().getDay()) < (b.get(0).getArrival().getDay())) ? 1 : ((a.get(0).getArrival().getDay()) > (b.get(0).getArrival().getDay())) ? -1 : 0);
-
+        Collections.sort(listofBargeConatiners, (a,b) -> ((a[0][0].get(0).getArrival().getDay()) < (b[0][0].get(0).getArrival().getDay())) ? 1 : ((a[0][0].get(0).getArrival().getDay()) > (b[0][0].get(0).getArrival().getDay())) ? -1 : 0);
+        
         while(trainContainers.size()>0){
             listoftrainContainers.add(new Stack<>());
             listoftrainContainers.get(listoftrainContainers.size()-1).push(trainContainers.get(0));
@@ -128,20 +148,29 @@ public class ContainingServer extends SimpleApplication
         Collections.sort(listoftruckContainers, (a,b) -> ((a.getArrival().getDay()) < (b.getArrival().getDay())) ? 1 : ((a.getArrival().getDay()) > (b.getArrival().getDay())) ? -1 : 0);
         
         while(seaShipContainers.size()>0){
-            listofSeaShipContainers.add(new Stack<>());
-            listofSeaShipContainers.get(listofSeaShipContainers.size()-1).push(seaShipContainers.get(0));
+           listofSeaShipContainers.add((Stack<Container>[][]) new Stack[21][6]);
+           for(int i = 0; i<21;i++){
+               for(int j = 0; j< 6;j++){
+                   listofSeaShipContainers.get(listofSeaShipContainers.size()-1)[i][j] = new Stack<>();
+               }
+           }
+           int x = (int)seaShipContainers.get(0).getPositie().x;
+           int z = (int)seaShipContainers.get(0).getPositie().z;
+           listofSeaShipContainers.get(listofSeaShipContainers.size()-1)[x][z].push(seaShipContainers.get(0));
             seaShipContainers.remove(0);
             isFull = false;
             while(seaShipContainers.size()>0 && !isFull){
                 if(seaShipContainers.get(0).getPositie().equals(Vector3f.ZERO)){
                     isFull = true;
                 }else{
-                    listofSeaShipContainers.get(listofSeaShipContainers.size()-1).push(seaShipContainers.get(0));
+                    x = (int)seaShipContainers.get(0).getPositie().x;
+                    z = (int)seaShipContainers.get(0).getPositie().z;
+                    listofSeaShipContainers.get(listofSeaShipContainers.size()-1)[x][z].push(seaShipContainers.get(0));
                     seaShipContainers.remove(0);
                 }
             }
         }
-        Collections.sort(listofSeaShipContainers, (a,b) -> ((a.get(0).getArrival().getDay()) < (b.get(0).getArrival().getDay())) ? 1 : ((a.get(0).getArrival().getDay()) > (b.get(0).getArrival().getDay())) ? -1 : 0);
+        Collections.sort(listofSeaShipContainers, (a,b) -> ((a[0][0].get(0).getArrival().getDay()) < (b[0][0].get(0).getArrival().getDay())) ? 1 : ((a[0][0].get(0).getArrival().getDay()) > (b[0][0].get(0).getArrival().getDay())) ? -1 : 0);
         
         
         //forced garbage collection (reduces RAM usage from 700MB  to 60 MB in case of xml7)
