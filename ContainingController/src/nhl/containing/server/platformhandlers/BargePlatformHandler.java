9fcce18c9d26ccfae093507abc857004ca88bc29
megagorilla <sanderenbart@gmail.com>
2014-12-15 13:54:41 +0100

    rename to barge from rivership

--- ContainingController/src/nhl/containing/server/platformhandlers/BargePlatformHandler.java
+++ ContainingController/src/nhl/containing/server/platformhandlers/BargePlatformHandler.java
@@ -18,28 +18,28 @@ import nhl.containing.server.util.XMLFileReader;
 public class BargePlatformHandler {
     Stack<Stack<XMLFileReader.Container>[][]> listofBargeConatiners = new Stack<>();
 
-    public BargePlatformHandler(ArrayList<XMLFileReader.Container> riverShipContainers) {
+    public BargePlatformHandler(ArrayList<XMLFileReader.Container> BargeContainers) {
         boolean isFull;
-        while(riverShipContainers.size()>0){
+        while(BargeContainers.size()>0){
             listofBargeConatiners.add((Stack<XMLFileReader.Container>[][]) new Stack[13][3]);
            for(int i = 0; i<13;i++){
                for(int j = 0; j< 3;j++){
                    listofBargeConatiners.get(listofBargeConatiners.size()-1)[i][j] = new Stack<>();
                }
            }
-           int x = (int)riverShipContainers.get(0).getPositie().x;
-           int z = (int)riverShipContainers.get(0).getPositie().z;
-            listofBargeConatiners.get(listofBargeConatiners.size()-1)[x][z].push(riverShipContainers.get(0));
-            riverShipContainers.remove(0);
+           int x = (int)BargeContainers.get(0).getPositie().x;
+           int z = (int)BargeContainers.get(0).getPositie().z;
+            listofBargeConatiners.get(listofBargeConatiners.size()-1)[x][z].push(BargeContainers.get(0));
+            BargeContainers.remove(0);
             isFull = false;
-            while(riverShipContainers.size()>0 && !isFull){
-                if(riverShipContainers.get(0).getPositie().equals(Vector3f.ZERO)){
+            while(BargeContainers.size()>0 && !isFull){
+                if(BargeContainers.get(0).getPositie().equals(Vector3f.ZERO)){
                     isFull = true;
                 }else{
-                    x = (int)riverShipContainers.get(0).getPositie().x;
-                    z = (int)riverShipContainers.get(0).getPositie().z;
-                    listofBargeConatiners.get(listofBargeConatiners.size()-1)[x][z].push(riverShipContainers.get(0));
-                    riverShipContainers.remove(0);
+                    x = (int)BargeContainers.get(0).getPositie().x;
+                    z = (int)BargeContainers.get(0).getPositie().z;
+                    listofBargeConatiners.get(listofBargeConatiners.size()-1)[x][z].push(BargeContainers.get(0));
+                    BargeContainers.remove(0);
                 }
             }
         }
