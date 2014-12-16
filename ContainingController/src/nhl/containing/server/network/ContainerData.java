5ccc5a85e4c0ce0124e30c54ada90bd6b8c902c4
megagorilla <sanderenbart@gmail.com>
2014-12-16 14:11:28 +0100

    added containerdata and seashipdata

---
+++ ContainingController/src/nhl/containing/server/network/ContainerData.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nhl.containing.server.network;
+
+import com.jme3.math.Vector3f;
+import com.jme3.network.AbstractMessage;
+import com.jme3.network.serializing.Serializable;
+
+/**
+ *
+ * @author Sander
+ */
+@Serializable
+public class ContainerData extends AbstractMessage {
+    public int containerID;
+    public Vector3f Location;
+
+    public ContainerData(){}
+    
+    public ContainerData(int containerID, Vector3f Location) {
+        this.containerID = containerID;
+        this.Location = Location;
+    }
+    
+}
